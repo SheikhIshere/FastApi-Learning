17cff510acc92c06e2439d28b70380263765621d
SheikhIshere <thesheikh255@gmail.com>
2026-03-17 17:36:02 +0600

    feat: sucessfully shift on production database eg:postgres from sqlite

--- five Authentication & Authorization/todo/database.py
+++ five Authentication & Authorization/todo/database.py
@@ -4,10 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # sqlite configuration
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" 
+SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:password@localhost:5432/todoapp"
 
 # engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) # this is for sqllite only
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
